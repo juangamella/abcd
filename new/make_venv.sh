806020f07fed37edb2fe71aa1e44183373a9c6fe
Juan Gamella <juangamella@gmail.com>
2020-10-14 11:35:31 +0200

    Removed automatic upgrade of causaldag

--- new/make_venv.sh
+++ new/make_venv.sh
@@ -3,5 +3,5 @@
 python3 -m venv ./venv
 source ./venv/bin/activate
 pip3 install -r requirements.txt
-pip3 install --no-cache-dir --upgrade causaldag
+#pip3 install --no-cache-dir --upgrade causaldag
 
